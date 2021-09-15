2bc26a998a04d4b26c0852e341688fc13f6975f2
Chanhee Park <parkchanhee@gmail.com>
2021-09-15 16:53:59 -0500

    .

--- src/lcc/main.cpp
+++ src/lcc/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <queue>
+#include <cassert>
+
+#include <unistd.h>
+#include <getopt.h>
+
 #include "node.h"
 
 extern NBlock* ProgramBlock;
@@ -7,6 +12,136 @@ extern int yyparse();
 extern int yylex_destroy();
 extern FILE* yyin;
 
+enum {
+    ARG_START = 256,
+    ARG_HELP,
+    ARG_VERBOSE,
+    ARG_DEBUG,
+    ARG_LIBRARY_PATH,
+    ARG_DATA_PATH,
+    ARG_OUTPUT_PREFIX,
+};
+
+
+class FOption {
+public:
+    FOption() {
+        Reset();
+    }
+
+    int Parse(int argc, char *argv[])
+    {
+        static const char *ShortOptions = "hvL:i:o:";
+        static const struct option LongOptions[] = {
+                {"help", no_argument, NULL, 'h'},
+                {"verbose", no_argument, NULL, 'v'},
+                {"datadir", required_argument, NULL, 'i'},
+                {"output", required_argument, NULL, 'o'},
+                {"debug", no_argument, NULL, ARG_DEBUG},
+                {NULL, 0, NULL, 0},
+        };
+
+        int opt;
+
+        while ((opt = getopt_long(argc, argv, ShortOptions, LongOptions, NULL)) != -1) {
+
+            switch(opt) {
+                case 'h':
+                case ARG_HELP:
+                    bShowHelp = true;
+                    return 0;
+
+                case 'v':
+                case ARG_VERBOSE:
+                    Verbose++;
+                    break;
+
+                case 'i':
+                case ARG_DATA_PATH:
+                    DataPaths.emplace_back(optarg);
+                    break;
+
+                case 'L':
+                    LibraryPaths.emplace_back(optarg);
+                    break;
+
+                case 'o':
+                case ARG_OUTPUT_PREFIX:
+                    OutputPrefix.assign(optarg);
+                    break;
+
+                case ARG_DEBUG:
+                    bDebug = true;
+                    break;
+
+                case '?':
+                default:
+                    return -1;
+            }
+        }
+
+        assert(!bShowHelp);
+
+        for (int i = optind; i < argc; i++) {
+            SourceFiles.emplace_back(argv[i]);
+        }
+
+        return 0;
+    }
+
+    void Reset() {
+        DataPaths.clear();
+        LibraryPaths.clear();
+        SourceFiles.clear();
+
+        OutputPrefix.clear();
+
+        bDebug = false;
+        Verbose = 0;
+        bShowHelp = false;
+    };
+
+
+    void Usage()
+    {
+        std::cerr << "Usage" << std::endl;
+    }
+
+    void Dump()
+    {
+        std::cerr << "bDebug: " << bDebug << std::endl;
+        std::cerr << "Verbose: " << Verbose << std::endl;
+        std::cerr << "bShowHelp: " << bShowHelp << std::endl;
+        std::cerr << "OutputPrefix: " << OutputPrefix << std::endl;
+
+        std::cerr << "DataPaths: " << std::endl;
+        for(const auto& p : DataPaths) {
+            std::cerr << "\t" << p << std::endl;
+        }
+
+        std::cerr << "LibraryPaths: " << std::endl;
+        for(const auto& p : LibraryPaths) {
+            std::cerr << "\t" << p << std::endl;
+        }
+
+        std::cerr << "SourceFiles: " << std::endl;
+        for(const auto& p : SourceFiles) {
+            std::cerr << "\t" << p << std::endl;
+        }
+
+    }
+
+public:
+    std::vector<std::string> DataPaths;
+    std::vector<std::string> LibraryPaths;
+    std::vector<std::string> SourceFiles;
+
+    std::string OutputPrefix;
+
+    bool bDebug;
+    int Verbose;
+    bool bShowHelp;
+};
 
 class FExperiment {
 public:
@@ -73,9 +208,6 @@ void ScanNodes(const NBlock* InProgramBlock)
     if (!InProgramBlock) {
         return;
     }
-
-
-
 }
 
 int main(int argc, char *argv[])
@@ -85,17 +217,41 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    yyin = fopen(argv[1], "r");
-    yyparse();
-    fclose(yyin);
-    yylex_destroy();
+    FOption Option;
+    if (Option.Parse(argc, argv)) {
+        Option.Usage();
+        return -1;
+    }
+    if (Option.bShowHelp) {
+        Option.Usage();
+        return 0;
+    }
 
-//    std::cout << ProgramBlock << std::endl;
+    if (Option.Verbose) {
+        Option.Dump();
+    }
 
-//    DumpNBlock(ProgramBlock);
+    for (const auto& SourceFile: Option.SourceFiles) {
+        ProgramBlock = nullptr;
+        yyin = fopen(SourceFile.c_str(), "r");
+        if (yyin == NULL) {
+            std::cerr << "Can't open file: " << SourceFile << std::endl;
+            continue;
+        }
 
-    TraversalNode(ProgramBlock);
+        yyparse();
+        fclose(yyin);
+        yylex_destroy();
 
-    delete ProgramBlock;
+        if (Option.bDebug) {
+            std::cout << ProgramBlock << std::endl;
+            DumpNBlock(ProgramBlock);
+        }
+
+        TraversalNode(ProgramBlock);
+
+        delete ProgramBlock;
+    }
+
     return 0;
 }
