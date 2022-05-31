ea73b6d63bee87fd97fa51734c39c8234c435af7
Austin Marckx <austinmarckx@gmail.com>
2022-05-31 17:37:47 -0500

    add current versions of unittests

---
+++ src/lcc/unittest.py
@@ -0,0 +1,40 @@
+import unittest
+from SimFunctions import ReactionEquations
+
+# Unit test for ReactionEquations class
+class TestReactionEquations(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_CheckRateAndConc(self):
+        # Test the CheckRateAndConc method for N_molecules = 1, 2, 3, 5, 7 
+
+        return
+
+    def test_MassActionReactionRate(self):
+        # Test the MassActionReactionRate method for N_molecules = 1, 2, 3, 5, 7 
+    
+        return
+
+    def test_Allostery(self):
+        # Test the Allostery method for N_molecules = 1, 2, 3, 5, 7
+
+        return
+
+    def test_Reaction(self):
+        # Test the Reaction method for N_molecules =  1, 2, 3, 5, 7
+        
+        return
+
+
+
+    
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
